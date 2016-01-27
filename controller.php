50f26bdc849cfda7c7ebbda7cb832a56cd4f67a4
Jesse von Doom <jesse@cashmusic.org>
2016-01-26 21:30:47 -0800

    universal byline / date formatting

--- controller.php
+++ controller.php
@@ -23,8 +23,9 @@
  ***************************************************************************************************/
 
 //require_once(__DIR__.'/definitions.php');
-
 require_once(__DIR__.'/lib/mustache/Mustache.php');
+$lemmy = new Mustache;
+
 $full_index = json_decode(file_get_contents(__DIR__.'/index.json'),true);
 $request_type = false;
 $request_options = false;
@@ -68,6 +69,67 @@ if (isset($_GET['p'])) {
 	}
 }
 
+
+/***************************************************************************************************
+ *
+ * HELPER FUNCTIONS
+ *
+ ***************************************************************************************************/
+
+/**
+ * Takes a datestamp or a string capable of being converted to a datestamp and
+ * returns a "23 minutes ago" type string for it. Now you can be cute like
+ * Twitter.
+ *
+ * @return string
+ */function formatTimeAgo($time,$long=false) {
+	if (is_string($time)) {
+		if (is_numeric($time)) {
+			$datestamp = (int) $time;
+		} else {
+			$datestamp = strtotime($time);
+		}
+	} else {
+		$datestamp = $time;
+	}
+	$seconds = floor((time() - $datestamp));
+	if ($seconds < 60) {
+		$ago_str = $seconds . ' seconds ago';
+	} else if ($seconds >= 60 && $seconds < 120) {
+		$ago_str = '1 minute ago';
+	} else if ($seconds >= 120 && $seconds < 3600) {
+		$ago_str = floor($seconds / 60) .' minutes ago';
+	} else if ($seconds >= 3600 && $seconds < 7200) {
+		$ago_str = '1 hour ago';
+	} else if ($seconds >= 7200 && $seconds < 86400) {
+		$ago_str = floor($seconds / 3600) .' hours ago';
+	} else if ($seconds >= 86400 && $seconds < 31536000) {
+		if ($long) {
+			$ago_str = date('l, F d', $datestamp);
+		} else {
+			$ago_str = date('d M', $datestamp);
+		}
+	} else {
+		if ($long) {
+			$ago_str = date('l, F d, Y', $datestamp);
+		} else {
+			$ago_str = date('d M, y', $datestamp);
+		}
+	}
+	return $ago_str;
+}
+
+function formatByline($author) {
+	global $lemmy;
+	if (file_exists(__DIR__.'/authors/'.$author.'.json')) {
+		$details = json_decode(file_get_contents(__DIR__.'/authors/'.$author.'.json'),true);
+		$template = file_get_contents(__DIR__.'/templates/byline.mustache');
+		return $lemmy->render($template, $details);
+	} else {
+		return '';
+	}
+}
+
 /***************************************************************************************************
  *
  * GET DATA AND RENDER PAGE
@@ -86,12 +148,15 @@ if ($request_type) {
 			$display_options['content'] = Markdown(file_get_contents(__DIR__.'/work/'.$display_options['id'].'.md'));
 			$work_details = json_decode(file_get_contents(__DIR__.'/work/'.$display_options['id'].'.json'),true);
 			if ($work_details) {
+				$display_options = array_merge($work_details,$display_options);
 				// build tags array
 				$tmp_array = array();
 				foreach ($work_details['tags'] as $tag) {
 					$tmp_array[]['tag'] = $tag;
 				}
 				$display_options['tags'] = $tmp_array;
+				$display_options['display_time'] = formatTimeAgo($work_details['date']);
+				$display_options['display_byline'] = formatByline($work_details['author_id']);
 				if (isset($work_details['template'])) {
 					$template = file_get_contents(__DIR__.'/templates/'.$work_details['template'].'.mustache');
 				} else {
@@ -149,6 +214,5 @@ if ($request_type) {
 }
 
 // pick the correct template and echo
-$lemmy = new Mustache;
 echo $lemmy->render($template, $display_options);
 ?>
