3dea80ff608eedbe5ca48ac15d7e8b8cdc441be7
Jesse von Doom <jesse@cashmusic.org>
2016-06-08 10:55:58 -0700

    json for full index

--- controller.php
+++ controller.php
@@ -132,7 +132,6 @@ if ($parsed_route) {
 				}
 			}
 			if ($display_options['json']) {
-				$full_index['work'][$display_options['id']]['permalink'] = $display_options['root_url'] . '/view/' . $display_options['id'];
 				$output = array(
 					"metadata" => $full_index['work'][$display_options['id']],
 					"content" => $display_options['content']
@@ -182,6 +181,16 @@ if ($parsed_route) {
 		 $display_options['authors'] = $loopable_authors;
 		 $display_options['tags'] = $full_index['tags']['list'];
 		 $template = 'all';
+		 if ($display_options['json']) {
+			 // JSON requested, so spit it out and exit (no template)
+			 $output = array(
+				"authors" => $display_options['authors'],
+				"tags" => 	 $display_options['tags'],
+				"work" => 	 $display_options['work']
+			);
+			 echo json_encode($output);
+			 exit();
+		 }
 	} else if ($parsed_route['type'] == 'tag') {
 		/*************************************************************************
 		 *
@@ -201,7 +210,6 @@ if ($parsed_route) {
 				// set the content
 				$work = array();
 				foreach ($full_index['tags']['index'][$display_options['tag']] as $work_id) {
-					$full_index['work'][$work_id]['permalink'] = $display_options['root_url'] . '/view/' . $work_id;
 					if ((!in_array($work_id,$features) && !$display_options['json']) || $display_options['json']) {
 						$work[] = $full_index['work'][$work_id];
 					}
@@ -265,7 +273,6 @@ if ($parsed_route) {
  				// set the content
  				$work = array();
  				foreach ($full_index['authors']['index'][$display_options['author_id']] as $work_id) {
-					$full_index['work'][$work_id]['permalink'] = $display_options['root_url'] . '/view/' . $work_id;
  					$work[] = $full_index['work'][$work_id];
 					$display_options['author_name'] = $full_index['work'][$work_id]['author_name'];
  				}
